e30db680f6912b0dc84d6e2111d7c81c1017fc84
lmunarim <lmunarim@gmail.com>
2025-05-18 18:37:58 -0300

    ajuste projeto de testes

--- src/Infrastructure/Services/fiap.Services/PedidoService.cs
+++ src/Infrastructure/Services/fiap.Services/PedidoService.cs
@@ -20,7 +20,7 @@ namespace fiap.Services
             {
                 var client = _httpClient.CreateClient("Pedidos");
                 //var url = $"api/pedidos/atualizar-status?idPedido={Uri.EscapeDataString(idPedido)}&statusPedido={Uri.EscapeDataString(statusPedido)}&statusPagamento={Uri.EscapeDataString(statusPagamento)}";
-                var response = await client.PutAsync($"api/pedidos/atualizar-status?idPedido={idPedido}&statusPedido={statusPedido}&statusPagamento={statusPagamento}", null);
+                var response = await client.PutAsync($"http://abf8a0373974c477c988a709a3916975-1818303021.us-east-1.elb.amazonaws.com/api/pedidos/atualizar-status?idPedido={idPedido}&statusPedido={statusPedido}&statusPagamento={statusPagamento}", null);
 
 
                 if (response.IsSuccessStatusCode)
