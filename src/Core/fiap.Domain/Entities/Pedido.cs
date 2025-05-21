23d70e2b7c267584db4536976dd2187042fbb34b
Gisele Cesar <gisele-cesar@hotmail.com>
2025-05-21 02:38:22 -0300

    Correcao propriedades classe Pedido

--- src/Core/fiap.Domain/Entities/Pedido.cs
+++ src/Core/fiap.Domain/Entities/Pedido.cs
@@ -8,8 +8,6 @@ namespace fiap.Domain.Entities
         public int IdPedido { get; set; }
         public Cliente Cliente { get; set; }
         public string Numero { get; set; }
-        public string StatusPedido { get; set; }
-        public string StatusPagamento { get; set; }
         public decimal ValorTotal
         {
             get { return Produtos?.Select(p => p.Preco).Sum() ?? 0; }
